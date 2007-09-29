cbdf359202b63db9bff3ff29ac097e1ba0f79127
atuining <atuining@0cf6dada-cf32-0410-b4fe-d86b42e8394d>
2007-09-29 18:07:49 +0000

    Tweak setup script to include dependent C files; remove the necessity of the win32api module on Windows and the MANIFEST and setup.cfg configuration files; include the Oracle version in the build directories; automatically search the lib32 and lib64 as needed on 32-bit and 64-bit platforms.
    git-svn-id: https://cx-oracle.svn.sourceforge.net/svnroot/cx-oracle/branches/4.3@58 0cf6dada-cf32-0410-b4fe-d86b42e8394d

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ Unix platforms
 
 """
 
+import sys
+
+if sys.platform == "win32" and sys.version_info[:2] >= (2, 5):
+    import distutils.command.bdist_msi
+    import distutils.command.bdist_wininst
+import distutils.command.build
+import distutils.dist
+import distutils.util
 import os
 import sys
 
@@ -16,22 +24,25 @@ from distutils.errors import DistutilsSetupError
 from distutils.extension import Extension
 
 # define build constants
-BUILD_VERSION = "4.3.2"
+BUILD_VERSION = "4.3.3"
 
 # define the list of files to be included as documentation for Windows
 dataFiles = None
 if sys.platform in ("win32", "cygwin"):
     baseName = "cx_Oracle-doc"
     dataFiles = [ (baseName, [ "LICENSE.TXT", "README.TXT", "HISTORY.txt"]) ]
-    allFiles = []
-    for fileName in open("MANIFEST").readlines():
-        allFiles.append(fileName.strip())
     for dir in ("html", "test"):
         files = []
-        for name in allFiles:
-            if name.startswith(dir):
-                files.append(name)
-        dataFiles.append( ("%s/%s" % (baseName, dir), files) )
+        fullDirName = "%s/%s" % (baseName,dir)
+        for name in os.listdir(dir):
+            if name.startswith("."):
+                continue
+            fullName = "%s/%s" % (dir, name)
+            files.append(fullName)
+        dataFiles.append((fullDirName, files))
+
+# define the list of files to be included as documentation for bdist_rpm
+docFiles = "LICENSE.txt README.txt HISTORY.txt html test"
 
 # try to determine the ORACLE_HOME
 oracleHome = os.environ.get("ORACLE_HOME")
@@ -49,10 +60,10 @@ if oracleHome is None:
 
 # define some variables
 if sys.platform == "win32":
-    libDirs = None
+    libDirs = [os.path.join(oracleHome, "bin")]
     includeDirs = [os.path.join(oracleHome, "oci", "include"), \
             os.path.join(oracleHome, "rdbms", "demo")]
-    libs = []
+    libs = ["oci"]
 elif sys.platform == "cygwin":
     includeDirs = ["/usr/include", "rdbms/demo", "rdbms/public", \
             "network/public", "oci/include"]
@@ -70,8 +81,12 @@ else:
     for i in range(len(includeDirs)):
         includeDirs[i] = os.path.join(oracleHome, includeDirs[i])
     libPath = os.path.join(oracleHome, "lib")
-    if sys.platform == "sunos5" and sys.maxint > 2147483647:
-        libPath = os.path.join(oracleHome, "lib64")
+    if sys.maxint == 2 ** 31 - 1:
+        alternatePath = os.path.join(oracleHome, "lib32")
+    else:
+        alternatePath = os.path.join(oracleHome, "lib64")
+    if os.path.exists(alternatePath):
+        libPath = alternatePath
     libDirs = [libPath, oracleHome]
     libs = ["clntsh"]
 
@@ -89,16 +104,95 @@ elif sys.platform == "cygwin":
     extraCompileArgs.append("-mno-cygwin")
 elif sys.platform == "darwin":
     extraLinkArgs = None
-elif sys.platform == "win32":
-    import win32api
-    extraLinkArgs.append(win32api.GetModuleFileName(sys.dllhandle))
-    extraLinkArgs.append(os.path.join(oracleHome, "bin", "oci.dll"))
 
 # force the inclusion of an RPATH linker directive if desired; this will
 # eliminate the need for setting LD_LIBRARY_PATH but it also means that this
 # location will be the only location searched for the Oracle client library
 if "FORCE_RPATH" in os.environ:
     extraLinkArgs.append("-Wl,-rpath,%s/lib" % oracleHome)
+
+# tweak distribution full name to include the Oracle version
+class Distribution(distutils.dist.Distribution):
+
+    def __init__(self, attrs):
+        global oracleHome
+        distutils.dist.Distribution.__init__(self, attrs)
+        if sys.platform == "win32":
+            subDir = "bin"
+            filesToCheck = [
+                    ("11g", "oraclient11.dll"),
+                    ("10g", "oraclient10.dll"),
+                    ("9i", "oraclient9.dll"),
+                    ("8i", "oraclient8.dll")
+            ]
+        else:
+            subDir = "lib"
+            filesToCheck = [
+                    ("11g", "libclient11.a"),
+                    ("10g", "libclient10.a"),
+                    ("9i", "libclient9.a"),
+                    ("8i", "libclient8.a")
+            ]
+        self.oracleVersion = None
+        for version, baseFileName in filesToCheck:
+            fileName = os.path.join(oracleHome, subDir, baseFileName)
+            if os.path.exists(fileName):
+                self.oracleVersion = version
+                break
+        if self.oracleVersion is None:
+            raise DistutilsSetupError, "Oracle home does not refer to an " \
+                    "8i, 9i, 10g or 11g installation"
+
+    def get_fullname_with_oracle_version(self):
+        name = self.metadata.get_fullname()
+        return "%s-%s" % (name, self.oracleVersion)
+
+
+# tweak the build directories to include the Oracle version
+class build(distutils.command.build.build):
+
+    def finalize_options(self):
+        global distutils
+        global os
+        global sys
+        platSpecifier = ".%s-%s-%s" % \
+                (distutils.util.get_platform(), sys.version[0:3],
+                 self.distribution.oracleVersion)
+        if self.build_platlib is None:
+            self.build_platlib = os.path.join(self.build_base,
+                    "lib%s" % platSpecifier)
+        if self.build_temp is None:
+            self.build_temp = os.path.join(self.build_base,
+                    "temp%s" % platSpecifier)
+        distutils.command.build.build.finalize_options(self)
+
+commandClasses = dict(build = build)
+
+# tweak the Windows installer names to include the Oracle version
+if sys.platform == "win32" and sys.version_info[:2] >= (2, 5):
+
+    class bdist_msi(distutils.command.bdist_msi.bdist_msi):
+
+        def run(self):
+            origMethod = self.distribution.get_fullname
+            self.distribution.get_fullname = \
+                    self.distribution.get_fullname_with_oracle_version
+            distutils.command.bdist_msi.bdist_msi.run(self)
+            self.distribution.get_fullname = origMethod
+
+    commandClasses["bdist_msi"] = bdist_msi
+
+    class bdist_wininst(distutils.command.bdist_wininst.bdist_wininst):
+
+        def run(self):
+            origMethod = self.distribution.get_fullname
+            self.distribution.get_fullname = \
+                    self.distribution.get_fullname_with_oracle_version
+            distutils.command.bdist_wininst.bdist_wininst.run(self)
+            self.distribution.get_fullname = origMethod
+
+    commandClasses["bdist_wininst"] = bdist_wininst
+
 
 # setup the extension
 extension = Extension(
@@ -108,15 +202,24 @@ extension = Extension(
         library_dirs = libDirs,
         extra_compile_args = extraCompileArgs,
         extra_link_args = extraLinkArgs,
-        sources = ["cx_Oracle.c"])
+        sources = ["cx_Oracle.c"],
+        depends = ["Callback.c", "Connection.c", "Cursor.c", "CursorVar.c",
+                "DateTimeVar.c", "Environment.c", "Error.c",
+                "ExternalDateTimeVar.c", "ExternalLobVar.c",
+                "ExternalObjectVar.c", "LobVar.c", "LongVar.c", "NumberVar.c",
+                "ObjectType.c", "ObjectVar.c", "SessionPool.c", "StringVar.c",
+                "TimestampVar.c", "Transforms.c", "Variable.c"])
 
 # perform the setup
 setup(
         name = "cx_Oracle",
         version = BUILD_VERSION,
+        distclass = Distribution,
         description = "Python interface to Oracle",
         license = "See LICENSE.txt",
         data_files = dataFiles,
+        cmdclass = commandClasses,
+        options = dict(bdist_rpm = dict(doc_files = docFiles)),
         long_description = \
             "Python interface to Oracle conforming to the Python DB API 2.0 "
             "specification.\n"
