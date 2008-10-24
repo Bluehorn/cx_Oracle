12ada47e5370fe64f65bdcf0cf391ca1fc66477e
atuining <atuining@0cf6dada-cf32-0410-b4fe-d86b42e8394d>
2008-10-24 17:12:53 +0000

    Make the bind variables and fetch variables accessible although they need to be treated carefully since they are used internally; support added for forward compatibility with version 5.x.
    git-svn-id: https://cx-oracle.svn.sourceforge.net/svnroot/cx-oracle/branches/4.4@192 0cf6dada-cf32-0410-b4fe-d86b42e8394d

--- Cursor.c
+++ Cursor.c
@@ -111,6 +111,8 @@ static PyMemberDef g_CursorMembers[] = {
     { "connection", T_OBJECT_EX, offsetof(udt_Cursor, connection), READONLY },
     { "numbersAsStrings", T_INT, offsetof(udt_Cursor, numbersAsStrings), 0 },
     { "rowfactory", T_OBJECT, offsetof(udt_Cursor, rowFactory), 0 },
+    { "bindvars", T_OBJECT, offsetof(udt_Cursor, bindVariables), READONLY },
+    { "fetchvars", T_OBJECT, offsetof(udt_Cursor, fetchVariables), READONLY },
     { NULL }
 };
 
